a83d4c796586b6dfac7d68dea0cd3301bee0d27f
Zorrochile15 <ju.elguetas@duocuc.cl>
2026-09-18 23:14:06 -0300

    local

---
+++ src/main/java/cl/duoc/pedidos360/backend/entity/Local.java
@@ -0,0 +1,32 @@
+package cl.duoc.pedidos360.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "locales")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Local {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(nullable = false, length = 200)
+    private String direccion;
+
+    @Column(length = 50)
+    private String telefono;
+}
