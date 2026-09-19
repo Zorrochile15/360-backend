d5775a4b84ae5347800cd150e2486987869b8e07
Zorrochile15 <ju.elguetas@duocuc.cl>
2026-09-18 23:09:56 -0300

    Insumo

---
+++ src/main/java/cl/duoc/pedidos360/backend/entity/Insumo.java
@@ -0,0 +1,37 @@
+package cl.duoc.pedidos360.backend.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "insumos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Insumo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(length = 255)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private BigDecimal precio;
+
+    @Column(nullable = false)
+    private Integer stock;
+}
