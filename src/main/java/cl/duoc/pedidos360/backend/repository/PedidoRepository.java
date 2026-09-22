0f2999d7797cd5d79b07c3675205b71b33ca1de2
NoahGonzalez <noa.gonzalez@duocuc.cl>
2026-09-22 15:58:51 -0300

    agrego consulta de pedidos para local

--- src/main/java/cl/duoc/pedidos360/backend/repository/PedidoRepository.java
+++ src/main/java/cl/duoc/pedidos360/backend/repository/PedidoRepository.java
@@ -1,5 +1,7 @@
 package cl.duoc.pedidos360.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import cl.duoc.pedidos360.backend.entity.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+    List<Pedido> findByLocalId(Long localId);
 }
